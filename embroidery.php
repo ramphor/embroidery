1fb858e0e426df4140a21e4c9607773898365ec0
Puleeno Nguyen <puleeno@gmail.com>
2019-07-16 16:58:56 +0700

    Added the plugin file

---
+++ embroidery.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Embroidery
+ * Author: Ramphor Premium
+ * Author: https://puleeno.com
+ * Version: 1.0.0
+ * Plugin URI: https://puleeno.com
+ * Description: Publish posts on frontend for WordPress
+ *
+ * @package Embroidery
+ */
+
+define( 'EMBROIDERY_PLUGIN_FILE', __FILE__ );
